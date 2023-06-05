6d1142777dab4760b24e8e86017f03cfcd5899e4
Kevin Campbell <khjc2012@gmail.com>
2023-06-04 20:56:31 -0500

    rasdasdas

--- base/models.py
+++ base/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class ExcelData(models.Model):
+    file = models.FileField(upload_to='excel_files/')
+    column1 = models.CharField(max_length=255)
+    column2 = models.CharField(max_length=255)
+    # Add more fields as per your requirements
--- testapp1/urls.py
+++ testapp1/urls.py
@@ -1,6 +1,11 @@
-from django.contrib import admin
+#from django.contrib import admin
 from django.urls import path
+from base.views import import_excel_view
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    path('import-excel/', import_excel_view, name='import-excel'),
+    path('', import_excel_view, name='import-excel'),  # Add this line
+    # Other URL patterns in your project
 ]
